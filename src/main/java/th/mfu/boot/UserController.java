7f83f712259c3b258842590d23fc296a2f6899af
LinnThitOakkar <6731503064@lamduan.mfu.ac.th>
2025-09-08 09:06:08 +0000

    done

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -1,5 +1,5 @@
 package th.mfu.boot;
-
+import java.util.Optional;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -17,39 +17,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     //TODO: add userrepository as `public` with @Autowired
     @Autowired
     public UserRepository repo;
    
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<String> registerUser(@RequestBody User user) {
+         User existingUser = repo.findByUsername(user.getUsername());
+        if (existingUser != null) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
 
-        //TODO: check if user with the username exists
-       
-        //TODO: save the user
-
-        //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        // Save new user
+        repo.save(user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public ResponseEntity<List<User>> list() {
-        
-        //TODO: remove below and return proper result
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+          List<User> users = repo.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+       
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
-        
-        //TODO: check if user with the id exists
-       
-        //TODO: delete the user
-    
-        //TODO: remove below and return proper status
-        return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
+        Optional<User> userOpt = repo.findById(id);
+        if (!userOpt.isPresent()) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+
+        repo.deleteById(id);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
     }
 
 
--- src/main/java/th/mfu/boot/UserRepository.java
+++ src/main/java/th/mfu/boot/UserRepository.java
@@ -5,5 +5,10 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+     @Override
+    List<User> findAll();
+
+    // Custom query method to find a user by username
+    User findByUsername(String username);
 
 }
